46beb4cd4f954695cc7d19b6b75428bb875d4a0c
Tacs <act.rtacangjr@gmail.com>
2025-05-24 15:12:19 +0800

    feat: Added catchAsync middleware

---
+++ server/src/middlewares/catchAsync.js
@@ -0,0 +1,7 @@
+const catchAsync = (cb) => async (req, res, next) => {
+    try {
+        return await cb(req, res, next)
+    } catch (error) {
+        next(createError(500, error.message))
+    }
+}
